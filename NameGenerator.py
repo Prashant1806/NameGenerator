6a74756543e4ac39d920ac8fd7ca6b732f123f1d
Prashant Goel <56596612+Prashant1806@users.noreply.github.com>
2019-10-22 20:31:42 +0530

    PC

---
+++ NameGenerator.py
@@ -0,0 +1,8 @@
+import random
+first = ("Super", "Retarded", "Great", "Sexy", "Vegan", "Brave", "Shy", "Cool", "Poor", "Rich", "Fast", "Gummy", "Yummy", "Masked", "Unusual", "American", "Bisexual", "MLG", "Mlg", "lil", "Lil")
+second = ("Coder", "Vegan", "Man", "Hacker", "Horse", "Bear", "Goat", "Goblin", "Learner", "Killer", "Woman", "Programmer", "Spy", "Stalker", "Spooderman", "Carrot", "Goat", "Quickscoper", "Quickscoper")
+firrst = random.choice(first)
+seccond = random.choice(second)
+name = (firrst + " " + seccond
+)
+print("Your name is: " + name)
